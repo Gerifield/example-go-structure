057997b2506f0d9d29d5bb280bf467dc4f07f70e
Gerifield <gergely.radics1@ibm.com>
2021-01-20 17:18:24 +0100

    feat: subcases for table testing

--- src/example1/middleware_test.go
+++ src/example1/middleware_test.go
@@ -40,38 +40,41 @@ func TestAuthMiddleware1ShouldAuth(t *testing.T) {
 	})
 
 	testTable := []struct {
+		name           string
 		user           string
 		pass           string
 		expectedCalled bool
 		expectedCode   int
 		expectedBody   string
 	}{
-		{"admin", "admin", true, 200, "ok"},
-		{"admin", "admin2", false, 200, ""}, // The code is wrong, but the middleware works like this here
-		{"", "", false, 200, ""},            // Should test the missing auth header, I've added an if for this case in the code
+		{"OK", "admin", "admin", true, 200, "ok"},
+		{"Wrong password", "admin", "admin2", false, 200, ""}, // The code is wrong, but the middleware works like this here
+		{"No header", "", "", false, 200, ""},                 // Should test the missing auth header, I've added an if for this case in the code
 	}
 
 	rec := httptest.NewRecorder()
 
 	for _, tt := range testTable {
-		// I've moved this var inside so I should not care about the basic auth header reset in the `if tt.user != "" {` block
-		req := httptest.NewRequest(http.MethodGet, "http://test", nil)
+		t.Run(tt.name, func(t *testing.T) {
+			// I've moved this var inside so I should not care about the basic auth header reset in the `if tt.user != "" {` block
+			req := httptest.NewRequest(http.MethodGet, "http://test", nil)
 
-		// Don't forget to reset the local variable and clean the buffers
-		handlerCalled = false
-		rec.Body.Reset()
+			// Don't forget to reset the local variable and clean the buffers
+			handlerCalled = false
+			rec.Body.Reset()
 
-		// Overwrite the old params with the new ones
-		if tt.user != "" {
-			req.SetBasicAuth(tt.user, tt.pass)
-		}
+			// Overwrite the old params with the new ones
+			if tt.user != "" {
+				req.SetBasicAuth(tt.user, tt.pass)
+			}
 
-		// Call the middleware
-		authMiddleware1(handler).ServeHTTP(rec, req)
+			// Call the middleware
+			authMiddleware1(handler).ServeHTTP(rec, req)
 
-		// Check the results
-		assert.Equal(t, tt.expectedCalled, handlerCalled)
-		assert.Equal(t, tt.expectedCode, rec.Code)
-		assert.Equal(t, tt.expectedBody, rec.Body.String())
+			// Check the results
+			assert.Equal(t, tt.expectedCalled, handlerCalled)
+			assert.Equal(t, tt.expectedCode, rec.Code)
+			assert.Equal(t, tt.expectedBody, rec.Body.String())
+		})
 	}
 }
